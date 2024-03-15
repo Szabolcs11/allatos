e75712ba9c08b58527c33804d3ac773fc95c0047
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-03-15 19:21:59 +0100

    Wrapping layout into html tag

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 // import { cookies } from 'next/headers';
-import Button from './Button';
 
 
 function Layout() {
-
-
   return (
+    <html>
     <section className="mt-16 mb-16 flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
       <h1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">      </h1>
         <div style={{display: "flex", gap: 32, alignItems: 'center'}}>
@@ -20,7 +18,6 @@ function Layout() {
             <Link href="/blog" >
               <p>Blogok</p>
             </Link>
-            <Button />
             {/* <div style={{display: 'flex', alignItems: 'center', gap: 16}}>
               <div>NYELV:</div>
               <div style={{display: 'flex', gap: 32}}>
@@ -30,8 +27,8 @@ function Layout() {
             </div> */}
           </div>
         </div>
-
-    </section>
+      </section>
+    </html>
   );
 }
 
